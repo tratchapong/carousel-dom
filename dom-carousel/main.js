5dce0118995efa38c536f25035d7bb019f6d402c
tratchapong <tratchapong@gmail.com>
2023-08-10 15:57:09 +0700

    carousel : bacl-next-auto ok

--- dom-carousel/main.js
+++ dom-carousel/main.js
@@ -1,8 +1,11 @@
 let slide = document.querySelector('.slides')
 let nextBtn = document.querySelector('.next-button')
 let backBtn = document.querySelector('.back-button')
+let autoSlide = document.querySelector('.auto-slide')
 let current = 0
 let numSlide = 4
+let autoNext
+
 
 nextBtn.addEventListener('click', () => {
     current = current === numSlide-1 ? 0 : current+1
@@ -14,3 +17,12 @@ backBtn.addEventListener('click', () => {
     let currentSlide = current * -100
     slide.style.left = `${currentSlide}%`   
 })
+
+autoSlide.addEventListener('change', () => {
+    console.log(autoSlide.checked)
+    if(autoSlide.checked) 
+        autoNext = setInterval(()=>nextBtn.click(), 2000)
+    else 
+        clearInterval(autoNext)
+})
+
