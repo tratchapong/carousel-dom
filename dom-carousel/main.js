72bd91a0137951ad94557e3d0c283e329420bcf4
tratchapong <tratchapong@gmail.com>
2023-08-10 15:24:56 +0700

    carousel : back-next

--- dom-carousel/main.js
+++ dom-carousel/main.js
@@ -1,10 +1,16 @@
 let slide = document.querySelector('.slides')
 let nextBtn = document.querySelector('.next-button')
+let backBtn = document.querySelector('.back-button')
 let current = 0
 let numSlide = 4
 
 nextBtn.addEventListener('click', () => {
     current = current === numSlide-1 ? 0 : current+1
+    let currentSlide = current * -100
+    slide.style.left = `${currentSlide}%`   
+})
+backBtn.addEventListener('click', () => {
+    current = current === 0 ? numSlide-1 : current-1
     let currentSlide = current * -100
     slide.style.left = `${currentSlide}%`   
 })
